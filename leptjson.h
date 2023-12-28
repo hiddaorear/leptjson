d41d07454467e943c3ab3cffbe954e8f8f0f4963
siegelwang <siegelwang@tencent.com>
2023-12-28 21:19:22 +0800

    feat: init

---
+++ leptjson.h
@@ -0,0 +1,26 @@
+//
+// Created by 汪怡峰 on 2023/12/28.
+//
+
+#ifndef LEPTJSON_LEPTJSON_H
+#define LEPTJSON_LEPTJSON_H
+
+
+typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+
+typedef struct {
+    lept_type type;
+}lept_value;
+
+enum {
+    LEPT_PARSE_OK = 0,
+    LEPT_PARSE_EXPECT_VALUE,
+    LEPT_PARSE_INVALID_VALUE,
+    LEPT_PARSE_ROOT_NOT_SINGULAR
+};
+
+int lept_parse(lept_value* v, const char* json);
+
+lept_type lept_get_type(const lept_value* v);
+
+#endif //LEPTJSON_LEPTJSON_H
